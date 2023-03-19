b0c8dbb5f6f050b99ebabec8599ce55a6907e41d
Okoyaki <max23340@gmail.com>
2023-03-19 16:46:06 +0500

    Update settings.py

--- python_base/Chapter 14/target_shoot/settings.py
+++ python_base/Chapter 14/target_shoot/settings.py
@@ -9,11 +9,9 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Настройки корабля
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Параметры снаряда
-        self.bullet_speed = 1
         self.bullet_width = 15
         self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
@@ -28,3 +26,18 @@ class Settings():
         self.target_color = (60, 60, 60)
         # target_direction = 1 обозначает движение вверх; а -1 - вниз.
         self.target_direction = -1
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.target_speed_factor = 1.0
+
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
